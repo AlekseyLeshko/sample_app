6ed25882e3e3d2e4a96f64a0d75bdfdf5b711ca9
alekseyleshko <alekseyleshko@gmail.com>
2013-10-26 01:23:43 +0400

    add coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 require 'rubygems'
 require 'spork'
+require 'simplecov'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However, 
+  # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
   # This file is copied to spec/ when you run 'rails generate rspec:install'
+  SimpleCov.start('rails')
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'

c0865dc22eb8b376c3b334339e1a2952d4d2f46e
alekseyleshko <alekseyleshko@gmail.com>
2013-11-14 03:15:57 +0400

    Update tests for micropost

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
-  before do
-    # Этот код кривой!
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
-  end
+  before { @micropost = user.microposts.build(content: "Lorem ipsum") }
 
   subject { @micropost }
 
